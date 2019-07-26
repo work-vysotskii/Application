3d350c2dafc09d47d2c66d1e540f1cac1c55634a
Global <Global@example.com>
2019-07-26 10:17:10 +0300

    Подготовлен к релизу

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -20,28 +20,29 @@ class ApplicationController extends Controller
     {
 
         $massage= [
-            'full_name.min' => 'имя короткое имя',
-            'full_name.string'=> 'введите имя',
-            'full_name.max'=>'максимальная длина 48 символов',
-            'username.min' => ' короткое имя',
-            'username.string'=> 'введите имя',
-            'username.max'=>'максимальная длина 48 символов',
-            'email' => 'неправильный email',
-            'password'=>'недопустимый пароль',
-            'password_cofirmation'=> 'неправильный повтор',
+        'full_name.min' => 'имя короткое имя',
+        'full_name.string'=> 'введите имя',
+        'full_name.max'=>'максимальная длина 48 символов',
+        'username.min' => ' короткое имя',
+        'username.string'=> 'введите имя',
+        'username.max'=>'максимальная длина 48 символов',
+        'email' => 'неправильный email',
+        'password'=>'недопустимый пароль',
+        'password.confirmed'=> 'неправильный повтор',
             ];
-        $this->validate($request,[
+
+        $rules=[
         'full_name' => 'string|min:3|max:48',
         'username' => 'required|min:3|max:200',
-        'password'=> 'required|min:6|max:20|password_cofirmation',
-        'password_cofirmation'=> 'password_cofirmation',
+        'password'=> 'required|min:6|max:20|confirmed',
         'email' => 'email',
-        ], $massage);
+            ];
+
+        $this->validate($request,$rules, $massage);
         $application = new App\Application;
         $application->full_name = $request->full_name;
         $application->username = $request->username;
         $application->password = $request->password;
-        $application->password_confirmation = password_confirmation;
         $application->email = $request->email;
         $application->save();
 
@@ -49,12 +50,30 @@ class ApplicationController extends Controller
     }
     public function edit(Request $request, $id)
     {
+        $massage= [
+        'full_name.min' => 'имя короткое имя',
+        'full_name.string'=> 'введите имя',
+        'full_name.max'=>'максимальная длина 48 символов',
+        'username.min' => ' короткое имя',
+        'username.string'=> 'введите имя',
+        'username.max'=>'максимальная длина 48 символов',
+        'email' => 'неправильный email',
+        'password'=>'недопустимый пароль',
+        'password.confirmed'=> 'неправильный повтор',
+        ];
 
+        $rules=[
+        'full_name' => 'string|min:3|max:48',
+        'username' => 'required|min:3|max:200',
+        'password'=> 'required|min:6|max:20|confirmed',
+        'email' => 'email',
+        ];
+
+        $this->validate($request,$rules, $massage);
         $application = App\Application::findOrFail($id);
         $application->full_name = $request->full_name;
         $application->username = $request->username;
         $application->password = $request->password;
-        //$application->password_confirmation =password_confirmation;
         $application->email = $request->email;
         $application->save();
         return $application;
